11957fdfab1e5f623529ef5f0f936614fde116eb
totolabrico <thomas-barbe@hotmail.com>
2025-04-29 15:37:02 +0200

    debug test_export

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -9,7 +9,7 @@ def test_export():
     partiels = Partiels()
     exec_path = os.environ.get("PARTIELS_EXECUTABLE")
     assert exec_path is not None, "PARTIELS_EXECUTABLE environment variable is not set"
-    partiels.setExecPath(exec_path)    
+    partiels.setExecPath(exec_path)
     partiels.setVampPath("/opt/Partiels/PlugIns")
     audiofile = pkg_resources.resource_filename(
             __name__, 'samples/patatine.wav'
