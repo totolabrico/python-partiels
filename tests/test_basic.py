b6fe6ca985fa8e15f18b8714e1ef3248b56d52c4
totolabrico <thomas-barbe@hotmail.com>
2025-04-30 14:41:15 +0200

    remove the partiels exec from env in linux build

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -12,9 +12,6 @@ def test_create_instance():
 
 def test_export():
     partiels = Partiels()
-    #exec_path = os.environ.get("PARTIELS_EXECUTABLE")
-    #assert exec_path is not None, "PARTIELS_EXECUTABLE environment variable is not set"
-    #partiels.setExecPath(exec_path)
     audiofile = pkg_resources.resource_filename(
             __name__, 'samples/patatine.wav'
         )
